28c1c4a9c36c584b10035bb26b567bbc6024795a
Alakh23 <121476873+Alakh23@users.noreply.github.com>
2024-02-19 20:09:37 +0530

    task2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import Task2 from './Task2'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Task2/>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Task2.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./task2.css";
+const Task2 = () => {
+  const [count, setCount] = useState(0); // Declaring the state variable which is gatting changed
+  const digIncrementFunc = () => {
+    setCount(count + 1);
+  };
+  const digDecrementFunc = () => {
+    if (count > 0) {
+      //Necessary to check whether count>0 or not
+      setCount(count - 1);
+    } else {
+      alert("Sorry, zero limit reached"); //Alert box will appear when count goes <0
+    }
+  };
+  const deleteDigitFunc = () => {
+    setCount(0);
+  };
+  return (
+    <>
+      <div className="t2MainContainer">
+        <div className="t2container">
+          <div className="t2ContainerPart1">
+            <h2>{count}</h2>
+          </div>
+          <div className="t2ContainerPart2">
+            <div className="t2ContainerPart2Inside1">
+              <button onClick={digIncrementFunc} className="t2Increment">
+                Increment
+              </button>
+              <button onClick={digDecrementFunc} className="t2Decrement">
+                Decrement
+              </button>
+            </div>
+            <div className="t2ContainerPart2Inside2">
+              <i
+                onClick={deleteDigitFunc}
+                className="fa-solid fa-trash"
+                style={{ color: "#FDFDFD" }}
+              ></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Task2;
